258f155bd6e7794ca08c5b01300c019d142edc31
CarlGustavAlbertDwarfsteinYung <curiousmentalmage@gmail.com>
2019-02-04 18:31:23 -0500

    walking character +camera following + swap between worlds working

--- Assets/Resources/Scripts/ElementoControls.cs
+++ Assets/Resources/Scripts/ElementoControls.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ElementoControls : MonoBehaviour
 {
+    public float maxSpeed = 1.0f;
+    public float jumpTakeOffSpeed = 1.0f;
+
+    Rigidbody2D body;
 
     GameObject charm;
     GameObject strange;
+    GameObject cam;
 
     // Start is called before the first frame update
     void Start()
     {
         charm = GameObject.Find("Charm");
         strange = GameObject.Find("Strange");
+        cam = GameObject.Find("Camera");
+
+        body = GetComponent<Rigidbody2D>();
 
         strange.SetActive(false);
     }
@@ -24,5 +32,21 @@ public class ElementoControls : MonoBehaviour
            charm.SetActive(!charm.activeSelf);
            strange.SetActive(!charm.activeSelf);
        }
+
+       if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += new Vector3(-0.1f, 0, 0);
+
+       } else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += new Vector3(0.1f, 0, 0);
+       }
+
+       if (Input.GetKeyDown(KeyCode.Space)) {
+           body.AddForce(new Vector2(0, 60));
+       }
+
+    }
+
+    void FixedUpdate(){
+        cam.transform.position = new Vector3(gameObject.transform.position.x, cam.transform.position.y, cam.transform.position.z);
     }
 }
