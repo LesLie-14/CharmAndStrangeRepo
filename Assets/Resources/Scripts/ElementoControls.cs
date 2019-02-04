6a7471c7dd3144b9732e55ed42d9bef5b57381e6
CarlGustavAlbertDwarfsteinYung <curiousmentalmage@gmail.com>
2019-02-04 18:04:49 -0500

    added swapping X and a new char

---
+++ Assets/Resources/Scripts/ElementoControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementoControls : MonoBehaviour
+{
+
+    GameObject charm;
+    GameObject strange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charm = GameObject.Find("Charm");
+        strange = GameObject.Find("Strange");
+
+        strange.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.X)) {
+           charm.SetActive(!charm.activeSelf);
+           strange.SetActive(!charm.activeSelf);
+       }
+    }
+}
